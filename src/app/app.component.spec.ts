e22b86a8b33abd14645f0e6bb3958ea60d3eb2fb
Sarath S <sarath.s@auriss.com>
2020-01-31 14:42:54 +0530

    Unit test initialized

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,6 +1,8 @@
 import { TestBed, async } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
+import { CommonLayoutComponent } from './layout/common-layout/common-layout.component';
+import { FullLayoutComponent } from './layout/full-layout/full-layout.component';
 
 describe('AppComponent', () => {
   beforeEach(async(() => {
@@ -9,7 +11,9 @@ describe('AppComponent', () => {
         RouterTestingModule
       ],
       declarations: [
-        AppComponent
+        AppComponent,
+        FullLayoutComponent,
+        CommonLayoutComponent
       ],
     }).compileComponents();
   }));
@@ -20,16 +24,28 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'lucyfrontend'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
+  it('should create the app', () => {
+    const fixture = TestBed.createComponent(FullLayoutComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('lucyfrontend');
+    expect(app).toBeTruthy();
   });
 
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('lucyfrontend app is running!');
+  it('should create the app', () => {
+    const fixture = TestBed.createComponent(CommonLayoutComponent);
+    const app = fixture.debugElement.componentInstance;
+    expect(app).toBeTruthy();
   });
+
+  // it(`should have as title 'lucyfrontend'`, () => {
+  //   const fixture = TestBed.createComponent(AppComponent);
+  //   const app = fixture.debugElement.componentInstance;
+  //   expect(app.title).toEqual('lucyfrontend');
+  // });
+
+  // it('should render title', () => {
+  //   const fixture = TestBed.createComponent(AppComponent);
+  //   fixture.detectChanges();
+  //   const compiled = fixture.debugElement.nativeElement;
+  //   expect(compiled.querySelector('.content span').textContent).toContain('lucyfrontend app is running!');
+  // });
 });
