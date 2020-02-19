cef8514e5cdfa11af52874acae9dea4ae0e16d52
Sarath S <sarath.s@auriss.com>
2020-02-19 13:17:44 +0530

    New user functionality added

--- src/app/admin/components/new-user/new-user.component.ts
+++ src/app/admin/components/new-user/new-user.component.ts
@@ -1,15 +1,32 @@
 import { Component, OnInit } from '@angular/core';
-
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 @Component({
   selector: 'app-new-user',
   templateUrl: './new-user.component.html',
   styleUrls: ['./new-user.component.scss']
 })
 export class NewUserComponent implements OnInit {
-
-  constructor() { }
+  userForm: FormGroup;
+  isSubmitted = false;
+  constructor(private formBuilder: FormBuilder) { }
 
   ngOnInit() {
+    this.userForm = this.formBuilder.group({
+      firstName: ['', Validators.compose([Validators.required])],
+      lastName: ['', Validators.compose([Validators.required])],
+      companyName: [''],
+      email: ['', Validators.compose([Validators.required, Validators.pattern('[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,3}$')])],
+      password: ['', Validators.compose([Validators.required, Validators.pattern('^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,}$'), Validators.minLength(8)])],
+      phoneNumber: ['', Validators.compose([Validators.required, Validators.pattern(''), Validators.minLength(10)])],
+    });
+  }
+  
+  userSubmit(){
+    this.isSubmitted = true;
+    if (this.userForm.invalid) {
+      return;
+    }
+    console.log("userForm",this.userForm.value)
   }
 
 }
