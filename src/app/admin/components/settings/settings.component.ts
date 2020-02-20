f85ef412bf57b0cbfecb3b2151027e9a98e22bcb
Venkatesan Mariyappan <venkatesan.m@auriss.com>
2020-02-20 14:49:35 +0530

    Settings page UI design

--- src/app/admin/components/settings/settings.component.ts
+++ src/app/admin/components/settings/settings.component.ts
@@ -1,12 +1,12 @@
 import { Component, OnInit } from '@angular/core';
-
+import * as globals from '../../../globals';
 @Component({
   selector: 'app-settings',
   templateUrl: './settings.component.html',
   styleUrls: ['./settings.component.scss']
 })
 export class SettingsComponent implements OnInit {
-
+  profile_bg = globals.profile_bg;
   constructor() { }
 
   ngOnInit() {
