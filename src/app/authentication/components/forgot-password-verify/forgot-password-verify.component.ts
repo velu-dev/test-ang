b5db752eb5417e1ebfe817f93e33499b786f41cd
Venkatesan Mariyappan <venkatesan.m@auriss.com>
2020-02-25 14:29:43 +0530

    Forgot password & verify password UI implementation Updated

--- src/app/authentication/components/forgot-password-verify/forgot-password-verify.component.ts
+++ src/app/authentication/components/forgot-password-verify/forgot-password-verify.component.ts
@@ -1,12 +1,12 @@
 import { Component, OnInit } from '@angular/core';
-
+import * as globals from '../../../globals';
 @Component({
   selector: 'app-forgot-password-verify',
   templateUrl: './forgot-password-verify.component.html',
   styleUrls: ['./forgot-password-verify.component.scss']
 })
 export class ForgotPasswordVerifyComponent implements OnInit {
-
+  logo = globals.logo;
   constructor() { }
 
   ngOnInit() {
--- src/app/authentication/components/forgot-password/forgot-password.component.ts
+++ src/app/authentication/components/forgot-password/forgot-password.component.ts
@@ -1,12 +1,12 @@
 import { Component, OnInit } from '@angular/core';
-
+import * as globals from '../../../globals';
 @Component({
   selector: 'app-forgot-password',
   templateUrl: './forgot-password.component.html',
   styleUrls: ['./forgot-password.component.scss']
 })
 export class ForgotPasswordComponent implements OnInit {
-
+  logo = globals.logo;
   constructor() { }
 
   ngOnInit() {
