ec23d9af897e438f3059326f5352323d8ed6976a
Sarath S <sarath.s@auriss.com>
2020-02-25 14:41:49 +0530

    forgot password functionality added

--- src/app/authentication/components/forgot-password-verify/forgot-password-verify.component.ts
+++ src/app/authentication/components/forgot-password-verify/forgot-password-verify.component.ts
@@ -1,15 +1,53 @@
 import { Component, OnInit } from '@angular/core';
-
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { CognitoService } from 'src/app/shared/services/cognito.service';
+import { AuthenticationService } from '../../services/authentication.service';
+import { Router } from '@angular/router';
+import { NgxSpinnerService } from 'ngx-spinner';
+import * as globals from '../../../globals';
+import * as  errors from '../../../shared/messages/errors'
+import * as  success from '../../../shared/messages/success'
 @Component({
   selector: 'app-forgot-password-verify',
   templateUrl: './forgot-password-verify.component.html',
   styleUrls: ['./forgot-password-verify.component.scss']
 })
 export class ForgotPasswordVerifyComponent implements OnInit {
-
-  constructor() { }
+  logo = globals.logo
+  forgotVerifyForm: FormGroup;
+  isSubmitted = false;
+  errorMessages = errors;
+  passwordFieldType: boolean;
+  passwordMatchStatus: boolean = false;
+  error: any;
+  constructor(private formBuilder: FormBuilder,
+    private cognitoService: CognitoService,
+    private authenticationService: AuthenticationService,
+    private router: Router,
+    private spinnerService: NgxSpinnerService) { }
 
   ngOnInit() {
+    this.forgotVerifyForm = this.formBuilder.group({
+      email: ['', Validators.compose([Validators.required, Validators.pattern('[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,3}$')])],
+      code: ['', Validators.compose([Validators.required, Validators.pattern('^[0-9]*$'), Validators.minLength(6), Validators.maxLength(6)])],
+      password: ['', Validators.compose([Validators.required, Validators.pattern('^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%._^&*-]).{8,}$'), Validators.minLength(8)])],
+      confirmPassword: ['', Validators.compose([Validators.required, Validators.pattern('^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%._^&*-]).{8,}$'), Validators.minLength(8)])],
+    });
   }
 
+  forgorVerifySubmit() {
+    this.isSubmitted = true;
+    if (this.forgotVerifyForm.invalid) {
+      return;
+    }
+    this.spinnerService.show()
+    this.cognitoService.forgotPasswordSubmit(this.forgotVerifyForm.value.email.toLowerCase(), this.forgotVerifyForm.value.code, this.forgotVerifyForm.value.password).subscribe(forgotRes => {
+      this.spinnerService.hide()
+      console.log("forgotRes", forgotRes)
+      this.router.navigate(['/login']);
+    }, error => {
+      this.spinnerService.hide()
+      console.log("error", error);
+    })
+  }
 }
--- src/app/authentication/components/forgot-password/forgot-password.component.ts
+++ src/app/authentication/components/forgot-password/forgot-password.component.ts
@@ -1,15 +1,44 @@
 import { Component, OnInit } from '@angular/core';
-
+import { AlertService } from 'src/app/shared/services/alert.service';
+import { NgxSpinnerService } from 'ngx-spinner';
+import { Router } from '@angular/router';
+import { AuthenticationService } from '../../services/authentication.service';
+import { CognitoService } from 'src/app/shared/services/cognito.service';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import * as globals from '../../../globals';
+import * as  errors from '../../../shared/messages/errors'
+import * as  success from '../../../shared/messages/success'
 @Component({
   selector: 'app-forgot-password',
   templateUrl: './forgot-password.component.html',
   styleUrls: ['./forgot-password.component.scss']
 })
 export class ForgotPasswordComponent implements OnInit {
-
-  constructor() { }
+  forgotPasswordForm: FormGroup;
+  isSubmitted = false;
+  passwordFieldType: boolean;
+  logo = globals.logo;
+  errorMessages = errors;
+  constructor(private router: Router, private authenticationService: AuthenticationService, private cognitoService: CognitoService, private formBuilder: FormBuilder, private spinnerService: NgxSpinnerService, private alertService: AlertService) { }
 
   ngOnInit() {
+    this.forgotPasswordForm = this.formBuilder.group({
+      email: ['', Validators.compose([Validators.required, Validators.email, Validators.pattern('[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,3}$')])],
+    });
+  }
+
+  forgotPasswordSubmit() {
+    this.isSubmitted = true;
+    if (this.forgotPasswordForm.invalid) {
+      return;
+    }
+
+    this.cognitoService.forgotPassword(this.forgotPasswordForm.value.email.toLowerCase()).subscribe(forgotRes => {
+      console.log("forgotRes", forgotRes)
+      this.router.navigate(['/forgotpassword-verify']);
+    }, error => {
+      console.log("error", error);
+    })
   }
 
 }
