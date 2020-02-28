0adfb498d5ede6f0d01137ac044500eca6ae2cda
Velusamy Venkatraman <velusamy.v@auriss.com>
2020-02-28 10:56:32 +0530

    Subscriber list

--- src/app/subscriber/components/user/user.component.ts
+++ src/app/subscriber/components/user/user.component.ts
@@ -4,7 +4,7 @@ import { MatSort } from '@angular/material/sort';
 import * as globals from '../../../globals';
 import { User } from './../../../shared/model/user.model';
 import { Role } from './../../../shared/model/role.model';
-import { UserService } from 'src/app/shared/services/user.service';
+import { UserService } from './../../service/user.service';
 import { Router } from '@angular/router';
 import { ExportService } from './../../../shared/services/export.service';
 import { MatTableDataSource } from '@angular/material/table';
@@ -31,12 +31,14 @@ export class UserComponent implements OnInit {
     this.screenWidth = window.innerWidth;
     this.roles = [];
     this.userService.getRoles().subscribe(response => {
-      response.data.map(role => {
-        if (!(role.role_name == "Admin")) {
-          this.roles.push(role)
-          this.selectedRoleId.push(role.id)
-        }
-      })
+      this.roles = response.data;
+      // response.data.map(role => {
+        
+        // if (!(role.role_name == "Admin")) {
+        //   this.roles.push(role)
+        //   this.selectedRoleId.push(role.id)
+        // }
+      // })
       this.getUser(this.selectedRoleId);
       this.roles.map(function (el) {
         var o = Object.assign({}, el);
