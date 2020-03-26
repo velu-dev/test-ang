3871a67d2f5a12bc9e839639fba85d30040051a2
Sarath S <sarath.s@auriss.com>
2020-03-26 11:28:46 +0530

    examiner setting functionality add

--- src/app/subscriber/examiner/examiner-setting/examiner-setting.component.ts
+++ src/app/subscriber/examiner/examiner-setting/examiner-setting.component.ts
@@ -1,15 +1,109 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Validators, FormBuilder, FormGroup } from '@angular/forms';
+import { CognitoService } from 'src/app/shared/services/cognito.service';
+import { Router } from '@angular/router';
+import { AlertService } from 'src/app/shared/services/alert.service';
+import { SubscriberUserService } from '../../service/subscriber-user.service';
+import { NgxSpinnerService } from 'ngx-spinner';
+import { User } from 'src/app/shared/model/user.model';
+import * as globals from '../../../globals'
+import * as  errors from '../../../shared/messages/errors'
 @Component({
   selector: 'app-examiner-setting',
   templateUrl: './examiner-setting.component.html',
   styleUrls: ['./examiner-setting.component.scss']
 })
 export class ExaminerSettingComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
+  profile_bg = globals.profile_bg;
+  user: User;
+  currentUser = {};
+  userForm: FormGroup;
+  userPasswrdForm: FormGroup;
+  errorMessages = errors
+  constructor(
+    private spinnerService: NgxSpinnerService,
+    private userService: SubscriberUserService,
+    private formBuilder: FormBuilder,
+    private alertService: AlertService,
+    private router: Router,
+    private cognitoService: CognitoService
+  ) {
+    this.userService.getProfile().subscribe(res => {
+      console.log("res obj", res)
+      this.user = res.data;
+      if (res.data.organization_type == 'INDV') {
+        res.data.company_name = '';
+      }
+      delete res.data.organization_type;
+      delete res.data.business_nature;
+      delete res.data.logo;
+      this.userForm.setValue(res.data)
+    })
   }
-
+  ngOnInit() {
+    this.userPasswrdForm = this.formBuilder.group({
+      current_password: ['', Validators.compose([Validators.required, Validators.pattern('^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[-=_~/`#?!@$%._^&*()"-,:;><|}{]).{8,}$'), Validators.minLength(8)])],
+      new_password: ['', Validators.compose([Validators.required, Validators.pattern('^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[-=_~/`#?!@$%._^&*()"-,:;><|}{]).{8,}$'), Validators.minLength(8)])],
+      confirmPassword: ['', Validators.compose([Validators.required, Validators.pattern('^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[-=_~/`#?!@$%._^&*()"-,:;><|}{]).{8,}$'), Validators.minLength(8)])]
+    })
+    this.userForm = this.formBuilder.group({
+      id: [''],
+      role_id: [''],
+      first_name: ['', Validators.compose([Validators.required, Validators.pattern('[A-Za-z]+'), Validators.maxLength(50)])],
+      last_name: ['', Validators.compose([Validators.required, Validators.pattern('[A-Za-z]+'), Validators.maxLength(50)])],
+      middle_name: ['', Validators.compose([Validators.pattern('[A-Za-z]+'), Validators.maxLength(50)])],
+      company_name: ["", Validators.compose([Validators.maxLength(100)])],
+      sign_in_email_id: [{ value: "", disabled: true }, Validators.compose([Validators.required, Validators.pattern('[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,3}$')])]
+    });
+  }
+  userformSubmit() {
+    this.isSubmit = true;
+    if (this.userForm.invalid) {
+      return;
+    }
+    this.userService.updateUser(this.userForm.value).subscribe(res => {
+      this.alertService.openSnackBar("Profile updated successfully", 'success');
+      this.isSubmit = false;
+    }, error => {
+      this.isSubmit = false;
+      console.log(error.message)
+      this.alertService.openSnackBar(error.message, 'error');
+    })
+  }
+  isTypePassword = true;
+  changeInputType() {
+    this.isTypePassword = !this.isTypePassword
+  }
+  isSubmit = false;
+  changePassword() {
+    this.isSubmit = true;
+    console.log(this.userPasswrdForm)
+    if (!(this.userPasswrdForm.value.new_password == this.userPasswrdForm.value.confirmPassword)) {
+      console.log("password miss match  ")
+      this.alertService.openSnackBar(this.errorMessages.passworddidnotMatch, "error");
+      return
+    }
+    if (this.userPasswrdForm.invalid) {
+      console.log("Not valid form ")
+      return;
+    }
+    this.spinnerService.show();
+    this.cognitoService.getCurrentUser().subscribe(user => {
+      console.log(user)
+      this.cognitoService.changePassword(user, this.userPasswrdForm.value.current_password, this.userPasswrdForm.value.new_password).subscribe(res => {
+        this.alertService.openSnackBar("Password successfully changed", "success");
+        this.cognitoService.logOut().subscribe(res => {
+          this.spinnerService.hide();
+          this.isSubmit = false;
+          this.router.navigate(['/'])
+        })
+      }, error => {
+        this.spinnerService.hide();
+        if (error.code == 'NotAuthorizedException') {
+          error.message = this.errorMessages.oldpasswordworng;
+        }
+        this.alertService.openSnackBar(error.message, "error");
+      })
+    })
+  }
 }
