9b04f6e814d5f2361c8d8b0af979c354990548af
Sarath S <sarath.s@auriss.com>
2020-03-26 10:41:58 +0530

    create dashboard & setting component

---
+++ src/app/subscriber/examiner/examiner-setting/examiner-setting.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-examiner-setting',
+  templateUrl: './examiner-setting.component.html',
+  styleUrls: ['./examiner-setting.component.scss']
+})
+export class ExaminerSettingComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
