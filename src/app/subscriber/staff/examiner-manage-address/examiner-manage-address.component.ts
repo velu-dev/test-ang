d3c80e1550ccde8f2c2bdd3af3f2a043fd47a175
Sarath S <sarath.s@auriss.com>
2020-03-30 12:44:50 +0530

    Staff - examiner manage address page add

---
+++ src/app/subscriber/staff/examiner-manage-address/examiner-manage-address.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-examiner-manage-address',
+  templateUrl: './examiner-manage-address.component.html',
+  styleUrls: ['./examiner-manage-address.component.scss']
+})
+export class ExaminerManageAddressComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
