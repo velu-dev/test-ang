7f8ecd2af937763b72060cfc3cb76ed1d03020f5
Sarath S <sarath.s@auriss.com>
2020-04-09 09:54:23 +0530

    manage location filter add

--- src/app/subscriber/staff/manage-location/manage-location.component.ts
+++ src/app/subscriber/staff/manage-location/manage-location.component.ts
@@ -5,16 +5,7 @@ import { Observable } from 'rxjs';
 import { startWith, map } from 'rxjs/operators';
 
 
-export interface PeriodicElement {
-  name: string;
-  type: string;
-  address: string;
-  phone: string;
-}
-export interface User {
-  name: string;
-}
-const ELEMENT_DATA: PeriodicElement[] = [
+const ELEMENT_DATA: any[] = [
   { name: 'SarathExaminer', type: 'Pharmacy', address: '2723  Mandan Road, California, MO, Missouri, 65018', phone: '816-269-6918' },
   { name: 'VenkatesnExaminer', type: 'Telehealth', address: '2723  Mandan Road, California, MO, Missouri, 65018', phone: '816-269-6918' },
   { name: 'RajanExaminer', type: 'Homeless Shelter', address: '2723  Mandan Road, California, MO, Missouri, 65018', phone: '816-269-6918' },
@@ -29,35 +20,21 @@ export class ManageLocationComponent implements OnInit {
   xls = globals.xls
   displayedColumns =
     ['name', 'type', 'address', 'phone', 'action'];
-  dataSource = ELEMENT_DATA;
+  dataSource:any = ELEMENT_DATA;
 
-  myControl = new FormControl();
-  options: User[] = [
-    { name: 'Mary' },
-    { name: 'Shelley' },
-    { name: 'Igor' }
-  ];
-  filteredOptions: Observable<User[]>;
-
-  constructor() { }
+ 
+  constructor() {
+    this.dataSource.sortingDataAccessor = (data, sortHeaderId) =>(typeof(data[sortHeaderId]) == 'string') && data[sortHeaderId].toLocaleLowerCase();
+   }
 
   ngOnInit() {
-    this.filteredOptions = this.myControl.valueChanges
-      .pipe(
-        startWith(''),
-        map(value => typeof value === 'string' ? value : value.name),
-        map(name => name ? this._filter(name) : this.options.slice())
-      );
-
-  }
-  displayFn(user: User): string {
-    return user && user.name ? user.name : '';
+  
   }
 
-  private _filter(name: string): User[] {
-    const filterValue = name.toLowerCase();
-
-    return this.options.filter(option => option.name.toLowerCase().indexOf(filterValue) === 0);
+  applyFilter(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSource.filter = filterValue.trim().toLowerCase();
   }
+
 
 }
