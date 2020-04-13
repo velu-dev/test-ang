18d112905b10996f6aa85fa50e9bf958b5882f20
Sarath S <sarath.s@auriss.com>
2020-04-13 11:07:38 +0530

    examiner location delete added

--- src/app/subscriber/staff/manage-location/manage-location.component.ts
+++ src/app/subscriber/staff/manage-location/manage-location.component.ts
@@ -9,18 +9,6 @@ import { ExaminerService } from '../../service/examiner.service';
 import { MatPaginator } from '@angular/material/paginator';
 import { AlertService } from 'src/app/shared/services/alert.service';
 
-const ELEMENT_DATA: any[] = [
-  {first_name: 1, type: 'Hydrogen', weight: 1.0079, symbol: 'H'},
-  {first_name: 2, type: 'Helium', weight: 4.0026, symbol: 'He'},
-  {first_name: 3, type: 'Lithium', weight: 6.941, symbol: 'Li'},
-  {first_name: 4, type: 'Beryllium', weight: 9.0122, symbol: 'Be'},
-  {first_name: 5, type: 'Boron', weight: 10.811, symbol: 'B'},
-  {first_name: 6, type: 'Carbon', weight: 12.0107, symbol: 'C'},
-  {first_name: 7, type: 'Nitrogen', weight: 14.0067, symbol: 'N'},
-  {first_name: 8, type: 'Oxygen', weight: 15.9994, symbol: 'O'},
-  {first_name: 9, type: 'Fluorine', weight: 18.9984, symbol: 'F'},
-  {first_name: 10, type: 'Neon', weight: 20.1797, symbol: 'Ne'},
-];
 @Component({
   selector: 'app-manage-location',
   templateUrl: './manage-location.component.html',
@@ -34,7 +22,7 @@ export class ManageLocationComponent implements OnInit {
   dataSource: any;
   @ViewChild(MatSort, { static: true }) sort: MatSort;
   @ViewChild(MatPaginator, { static: true }) paginator: MatPaginator;
-  constructor(private examinerService: ExaminerService,private alertService: AlertService) {
+  constructor(private examinerService: ExaminerService, private alertService: AlertService) {
 
   }
 
@@ -56,13 +44,17 @@ export class ManageLocationComponent implements OnInit {
     this.dataSource.filter = filterValue.trim().toLowerCase();
   }
 
-  deleteAddress(data) {
-    let details= {
+  deleteAddress(data, index) {
+    let details = {
       user_id: data.examiner_id,
       address_id: data.address_id
     }
+    console.log(index)
     this.examinerService.PostDeleteExaminerAddress(details).subscribe(response => {
       console.log(response)
+      let tabledata = this.dataSource._data.value;
+      tabledata.splice(index, 1)
+      this.dataSource = new MatTableDataSource(tabledata);
       this.alertService.openSnackBar("Location deleted successfully", 'success');
 
     }, error => {
