1ab8e657fe48f78bf195fc2fa0572e7c09a64a87
Sarath S <sarath.s@auriss.com>
2020-04-24 12:43:41 +0530

    session issue fix

--- src/app/shared/guard/session.guard.ts
+++ src/app/shared/guard/session.guard.ts
@@ -12,60 +12,62 @@ export class SessionGuard implements CanActivate {
   canActivate(
     next: ActivatedRouteSnapshot,
     state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
-   
-      return this.cognitoService.session().then(token => {
-       
-          let role = this.cookieService.get('role_id')
-          if (role) {
-            console.log('session')
-            switch (role) {
-              case '1':
-                this.router.navigate(["/admin"]);
-                break;
-              case '2':
-                this.router.navigate(["/subscriber"]);
-                break;
-              case '3':
-                this.router.navigate(["/subscriber/manager"]);
-                break;
-              case '4':
-                this.router.navigate(["/subscriber/staff"]);
-                break;
-              case '5':
-                this.router.navigate(["/vendor/historian"]);
-                break;
-              case '6':
-                this.router.navigate(["/vendor/historian/staff"]);
-                break;
-              case '7':
-                this.router.navigate(["/vendor/summarizer"]);
-                break;
-              case '8':
-                this.router.navigate(["/vendor/summarizer/staff"]);
-                break;
-              case '9':
-                this.router.navigate(["/vendor/transcriber"]);
-                break;
-              case '10':
-                this.router.navigate(["/vendor/transcriber/staff"]);
-                break;
-              default:
-                this.router.navigate(["/"]);
-                break;
 
-            }
+    return this.cognitoService.session().then(token => {
+
+      let role = this.cookieService.get('role_id')
+      if (role) {
+        console.log('session', role)
+        switch (role) {
+          case '1':
+            this.router.navigate(["/admin"]);
+            break;
+          case '2':
+            this.router.navigate(["/subscriber"]);
+            break;
+          case '3':
+            this.router.navigate(["/subscriber/manager"]);
+            break;
+          case '4':
+            this.router.navigate(["/subscriber/staff"]);
+            break;
+          // case '5':
+          //   this.router.navigate(["/vendor/historian"]);
+          //   break;
+          // case '6':
+          //   this.router.navigate(["/vendor/historian/staff"]);
+          //   break;
+          // case '7':
+          //   this.router.navigate(["/vendor/summarizer"]);
+          //   break;
+          // case '8':
+          //   this.router.navigate(["/vendor/summarizer/staff"]);
+          //   break;
+          // case '9':
+          //   this.router.navigate(["/vendor/transcriber"]);
+          //   break;
+          // case '10':
+          //   this.router.navigate(["/vendor/transcriber/staff"]);
+          //   break;
+          case '11':
+            this.router.navigate(["/subscriber/examiner"]);
+            break;
+          default:
+            this.cookieService.deleteAll()
             return true;
-          } else {
-            return true;
-          }
-      
-      }, error => {
-        console.log(error);
+        }
+      } else {
         this.cookieService.deleteAll()
         return true;
-      })
+      }
 
-   
+    }, error => {
+      console.log(error);
+      this.cookieService.deleteAll()
+      return true;
+    })
+
+
   }
 
 }
