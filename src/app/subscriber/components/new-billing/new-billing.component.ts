8148e05f676413c8e361ca9aca00dee68bf27012
Venkatesan Mariyappan <venkatesan.m@auriss.com>
2020-05-13 15:59:05 +0530

    Create billing UI implementation

--- src/app/subscriber/components/new-billing/new-billing.component.ts
+++ src/app/subscriber/components/new-billing/new-billing.component.ts
@@ -3,6 +3,8 @@ import { FormControl } from '@angular/forms';
 import { Observable } from 'rxjs';
 import { startWith, map } from 'rxjs/operators';
 import { MatTableDataSource, MatSort } from '@angular/material';
+import * as globals from '../../../globals';
+
 export interface PeriodicElement {
   item: string;
   procedure_code: string;
@@ -14,7 +16,18 @@ export interface PeriodicElement {
 export interface DocList {
   name: string;
   type: string;
- 
+
+}
+
+export interface PaymantList {
+  item: string;
+  procedure_code: string;
+  modifer: string;
+  units: string;
+  charge: string;
+  fee_schedule: string;
+  payment: string;
+  balance: string;
 }
 @Component({
   selector: 'app-new-billing',
@@ -22,10 +35,13 @@ export interface DocList {
   styleUrls: ['./new-billing.component.scss']
 })
 export class NewBillingComponent implements OnInit {
+  docx = globals.docx;
   displayedColumns: string[] = ['item', 'procedure_code', 'modifer', 'units', 'charge', 'fee_schedule'];
   displayedColumns1: string[] = ['name', 'type'];
+  displayedColumns2: string[] = ['item', 'procedure_code', 'modifer', 'units', 'charge', 'fee_schedule', 'payment', 'balance'];
   dataSource = new MatTableDataSource(ELEMENT_DATA);
   dataSource1 = new MatTableDataSource(ELEMENT_DATA1);
+  dataSource2 = new MatTableDataSource(ELEMENT_DATA2);
   @ViewChild(MatSort, { static: true }) sort: MatSort;
   myControl = new FormControl();
   options: string[] = ['123456', 'M Venkat', 'M Rajan'];
@@ -48,10 +64,17 @@ export class NewBillingComponent implements OnInit {
 }
 
 const ELEMENT_DATA: PeriodicElement[] = [
-  { item: 'QME Examination', procedure_code: '', modifer: '', units: '', charge: '$ 2200.00', fee_schedule: '$ 2200.00', },
+  { item: 'QME Examination', procedure_code: 'ML 101', modifer: '93', units: '', charge: '$ 2200.00', fee_schedule: '$ 2200.00', },
   { item: 'Report pages', procedure_code: '', modifer: '', units: '', charge: '$ 2200.00', fee_schedule: '$ 2200.00', }
 ];
 
 const ELEMENT_DATA1: DocList[] = [
-  { name:'gdgd', type:'dfdfdfdf' },
+  { name: 'steveson-123ABC-QME.pdf', type: 'Report' },
+  { name: 'submission-cover-letter-8765543.pdf', type: 'Attachment' },
 ];
+
+const ELEMENT_DATA2: PaymantList[] = [
+  { item: 'QME Examination', procedure_code: 'ML 101', modifer: '93', units: '1', charge: '$ 2200.00', fee_schedule: '$ 2200.00', payment: '$ 0', balance: '$ 2200.00' },
+  { item: 'Report pages', procedure_code: '', modifer: '', units: '', charge: '$ 2200.00', fee_schedule: '$ 2200.00', payment: '$ 0', balance: '$ 2200.00' }
+];
+
