b6a289f9baa6b1fe57ad7fdff556e88411d7ec3f
Venkatesan Mariyappan <venkatesan.m@auriss.com>
2020-05-13 18:06:15 +0530

    Edit billing page UI implementation

--- src/app/subscriber/components/edit-billing/edit-billing.component.ts
+++ src/app/subscriber/components/edit-billing/edit-billing.component.ts
@@ -1,5 +1,35 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 
+import * as globals from '../../../globals';
+import { MatTableDataSource, MatSort } from '@angular/material';
+import { FormControl } from '@angular/forms';
+import { Observable } from 'rxjs';
+import { startWith, map } from 'rxjs/operators';
+
+export interface PeriodicElement {
+  item: string;
+  procedure_code: string;
+  modifer: string;
+  units: string;
+  charge: string;
+  fee_schedule: string;
+}
+export interface DocList {
+  name: string;
+  type: string;
+
+}
+
+export interface PaymantList {
+  item: string;
+  procedure_code: string;
+  modifer: string;
+  units: string;
+  charge: string;
+  fee_schedule: string;
+  payment: string;
+  balance: string;
+}
 @Component({
   selector: 'app-edit-billing',
   templateUrl: './edit-billing.component.html',
@@ -7,9 +37,48 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EditBillingComponent implements OnInit {
 
+  docx = globals.docx;
+  displayedColumns: string[] = ['item', 'procedure_code', 'modifer', 'units', 'charge', 'fee_schedule'];
+  displayedColumns1: string[] = ['name', 'type'];
+  displayedColumns2: string[] = ['item', 'procedure_code', 'modifer', 'units', 'charge', 'fee_schedule', 'payment', 'balance'];
+  dataSource = new MatTableDataSource(ELEMENT_DATA);
+  dataSource1 = new MatTableDataSource(ELEMENT_DATA1);
+  dataSource2 = new MatTableDataSource(ELEMENT_DATA2);
+  @ViewChild(MatSort, { static: true }) sort: MatSort;
+  myControl = new FormControl();
+  options: string[] = ['123456', 'M Venkat', 'M Rajan'];
+  filteredOptions: Observable<string[]>;
+  filteredICD: Observable<[]>;
+
   constructor() { }
 
-  ngOnInit() {
+  ngOnInit() {   this.dataSource.sort = this.sort;
+    this.filteredOptions = this.myControl.valueChanges.pipe(
+      startWith(''),
+      map(value => this._filter(value))
+    );
+  }
+  private _filter(value: string): string[] {
+    const filterValue = value.toLowerCase();
+
+    return this.options.filter(option => option.toLowerCase().indexOf(filterValue) === 0);
   }
 
 }
+
+
+const ELEMENT_DATA: PeriodicElement[] = [
+  { item: 'QME Examination', procedure_code: 'ML 101', modifer: '93', units: '', charge: '$ 2200.00', fee_schedule: '$ 2200.00', },
+  { item: 'Report pages', procedure_code: '', modifer: '', units: '', charge: '$ 2200.00', fee_schedule: '$ 2200.00', }
+];
+
+const ELEMENT_DATA1: DocList[] = [
+  { name: 'steveson-123ABC-QME.pdf', type: 'Report' },
+  { name: 'submission-cover-letter-8765543.pdf', type: 'Attachment' },
+];
+
+const ELEMENT_DATA2: PaymantList[] = [
+  { item: 'QME Examination', procedure_code: 'ML 101', modifer: '93', units: '1', charge: '$ 2200.00', fee_schedule: '$ 2200.00', payment: '$ 0', balance: '$ 2200.00' },
+  { item: 'Report pages', procedure_code: '', modifer: '', units: '', charge: '$ 2200.00', fee_schedule: '$ 2200.00', payment: '$ 0', balance: '$ 2200.00' }
+];
+
