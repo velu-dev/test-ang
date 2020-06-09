a6a4524cf2c6c7e3c6eb08f165ddff963974efcd
Sarath S <sarath.s@auriss.com>
2020-06-09 08:36:48 +0530

    admin bug & injury cancel button changes

--- src/app/subscriber/components/claims/new-claim/new-claim.component.ts
+++ src/app/subscriber/components/claims/new-claim/new-claim.component.ts
@@ -1227,4 +1227,8 @@ export class NewClaimComponent implements OnInit {
     })
 
   }
+
+  injuryCancel(){
+    this.injuryInfo = { body_part_id: null, date_of_injury: null, continuous_trauma: false, continuous_trauma_start_date: null, continuous_trauma_end_date: null, injury_notes: null, diagram_url: null } 
+  }
 }
