4419db05d118f7426f5ed2c864a7352a5169c2aa
Sarath S <sarath.s@auriss.com>
2020-06-19 15:04:04 +0530

    file type chnages

--- src/app/shared/pipes/file-type.pipe.ts
+++ src/app/shared/pipes/file-type.pipe.ts
@@ -11,7 +11,7 @@ export class FileTypePipe implements PipeTransform {
   transform(value: any, ...args: any[]): any {
    if(value){
      let fileNanme = value.split('.').pop().toLowerCase();
-     if(fileNanme == 'xls' || fileNanme == 'xlsx') return this.xls;
+     if(fileNanme == 'xls' || fileNanme == 'xlsx') return this.xls_1;
      else if(fileNanme == 'docx' || fileNanme == 'doc') return this.docx;
      else if(fileNanme == 'pdf') return this.pdf;
      else if(fileNanme == 'csv') return this.xls_1;
