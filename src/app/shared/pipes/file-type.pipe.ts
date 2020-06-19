3669c6ec876357c75bd1724cb020a1a4cad02ff1
Sarath S <sarath.s@auriss.com>
2020-06-19 15:33:01 +0530

    file image changes

--- src/app/shared/pipes/file-type.pipe.ts
+++ src/app/shared/pipes/file-type.pipe.ts
@@ -4,18 +4,19 @@ import * as globals from './../../globals';
   name: 'fileType'
 })
 export class FileTypePipe implements PipeTransform {
-  xls = globals.xls;
-  xls_1 = globals.xls_1
-  docx = globals.docx
-  pdf = globals.pdf
+  xls_1 = globals.xls_1;
+  docx = globals.docx;
+  pdf = globals.pdf;
+  csv = globals.csv;
+  empty = globals.file;
   transform(value: any, ...args: any[]): any {
    if(value){
      let fileNanme = value.split('.').pop().toLowerCase();
      if(fileNanme == 'xls' || fileNanme == 'xlsx') return this.xls_1;
      else if(fileNanme == 'docx' || fileNanme == 'doc') return this.docx;
      else if(fileNanme == 'pdf') return this.pdf;
-     else if(fileNanme == 'csv') return this.xls_1;
-     else return this.docx
+     else if(fileNanme == 'csv') return this.csv;
+     else return this.empty
    }
     return null;
   }
