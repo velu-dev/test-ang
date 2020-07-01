3de3ac6b797f2bf8a415838f4e6493abc8f30bdf
Venkatesan Mariyappan <venkatesan.m@auriss.com>
2020-07-01 15:01:28 +0530

    convenience pages Upcoming Appointments, Unfinished Reports & Billing and Collections pages added

--- src/app/subscriber/components/dashboard/billing-collection/billing-collection.component.ts
+++ src/app/subscriber/components/dashboard/billing-collection/billing-collection.component.ts
@@ -1,15 +1,68 @@
 import { Component, OnInit } from '@angular/core';
+import { animate, state, style, transition, trigger } from '@angular/animations';
+import { Observable } from 'rxjs';
+import { shareReplay, map } from 'rxjs/operators';
+import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
 
 @Component({
   selector: 'app-billing-collection',
   templateUrl: './billing-collection.component.html',
-  styleUrls: ['./billing-collection.component.scss']
+  styleUrls: ['./billing-collection.component.scss'],
+  animations: [
+    trigger('detailExpand', [
+      state('collapsed', style({ height: '0px', minHeight: '0' })),
+      state('expanded', style({ height: '*' })),
+      transition('expanded <=> collapsed', animate('225ms cubic-bezier(0.4, 0.0, 0.2, 1)')),
+    ]),
+  ],
 })
 export class BillingCollectionComponent implements OnInit {
+  isHandset$: Observable<boolean> = this.breakpointObserver.observe(Breakpoints.Handset)
+  .pipe(
+    map(result => result.matches),
+    shareReplay()
+  );
+dataSource = ELEMENT_DATA;
+columnsToDisplay = [];
+expandedElement;
+isMobile = false;
+columnName = [];
+filterValue: string;
+constructor(private breakpointObserver: BreakpointObserver) {
+  this.isHandset$.subscribe(res => {
+    this.isMobile = res;
+    if (res) {
+      this.columnName = ["", "Claimant", "Action"]
+      this.columnsToDisplay = ['is_expand', 'claimant_name', "disabled"]
+    } else {
+      this.columnName = ["Claimant","Procedure Type", "Examiner", "Date of service / Date Item Received", "Bill Date", "Status"]
+      this.columnsToDisplay = ['claimant_name','procedure_type', "examiner", "dos", 'bill_date', 'status']
+    }
+  })
+}
 
-  constructor() { }
-
-  ngOnInit() {
+ngOnInit() {
+}
+expandId: any;
+openElement(element) {
+  if (this.isMobile) {
+    this.expandId = element.id;
   }
+}
+applyFilter(filterValue: string) {
+  // this.dataSource.filter = filterValue.trim().toLowerCase();
+  // if (this.dataSource.paginator) {
+  //   this.dataSource.paginator.firstPage();
+  // }
+}
 
 }
+const ELEMENT_DATA = [
+{ "id": 132, "last_name": "Mariyappan", "first_name": "Venkatesan", "procedure_type": "Examination", "ex_lastname": "Jorge","ex_firstname": "Sanchez","ex_suffix": "M.D.", "dos": "12-10-2020","bill_date":"10-25-2020", "status":"Not Sent",},
+{ "id": 132, "last_name": "Mariyappan", "first_name": "Venkatesan", "procedure_type": "Examination", "ex_lastname": "Jorge","ex_firstname": "Sanchez","ex_suffix": "M.D.", "dos": "12-10-2020","bill_date":"10-25-2020", "status":"Accepted"},
+{ "id": 132, "last_name": "Mariyappan", "first_name": "Venkatesan", "procedure_type": "Examination", "ex_lastname": "Jorge","ex_firstname": "Sanchez","ex_suffix": "M.D.", "dos": "12-10-2020","bill_date":"10-25-2020", "status":"Sent"},
+{ "id": 132, "last_name": "Mariyappan", "first_name": "Venkatesan", "procedure_type": "Examination", "ex_lastname": "Jorge","ex_firstname": "Sanchez","ex_suffix": "M.D.", "dos": "12-10-2020","bill_date":"10-25-2020", "status":"Not Sent"},
+{ "id": 132, "last_name": "Mariyappan", "first_name": "Venkatesan", "procedure_type": "Examination", "ex_lastname": "Jorge","ex_firstname": "Sanchez","ex_suffix": "M.D.", "dos": "12-10-2020","bill_date":"10-25-2020", "status":"Accepted"},
+{ "id": 132, "last_name": "Mariyappan", "first_name": "Venkatesan", "procedure_type": "Examination", "ex_lastname": "Jorge","ex_firstname": "Sanchez","ex_suffix": "M.D.", "dos": "12-10-2020","bill_date":"10-25-2020", "status":"Not Sent"},
+
+];
