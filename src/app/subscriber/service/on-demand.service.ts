86a6f0fa925b12f30b33ac5e8b88d760edf6c79d
Sarath S <sarath.s@auriss.com>
2020-07-28 14:08:11 +0530

    onDemand service added

---
+++ src/app/subscriber/service/on-demand.service.ts
@@ -0,0 +1,15 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable, ObservedValueOf } from 'rxjs';
+import { environment } from 'src/environments/environment';
+import { api_endpoint } from 'src/environments/api_endpoint';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class OnDemandService {
+
+  constructor(private http: HttpClient) {
+
+  }
+}
