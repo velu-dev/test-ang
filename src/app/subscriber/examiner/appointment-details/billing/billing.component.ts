934af1003c90bc71da63980af5ddf8329fb99d19
Venkatesan Mariyappan <venkatesan.m@auriss.com>
2020-08-20 16:06:35 +0530

    Billing page UI implementation

--- src/app/subscriber/examiner/appointment-details/billing/billing.component.ts
+++ src/app/subscriber/examiner/appointment-details/billing/billing.component.ts
@@ -21,16 +21,26 @@ export class BilllableBillingComponent implements OnInit {
       map(result => result.matches),
       shareReplay()
     );
-  dataSource = ELEMENT_DATA;
-  dataSource1 = ELEMENT_DATA1;
   columnsToDisplay = [];
-  columnsToDisplay1 = [];
+  dataSource = ELEMENT_DATA;
   expandedElement;
-  expandedElement1;
   isMobile = false;
-  isMobile1 = false;
   columnName = [];
+  dataSource1 = ELEMENT_DATA1;
+  columnsToDisplay1 = [];
+  expandedElement1;
   columnName1 = [];
+  isMobile1 = false;
+  dataSource2 = ELEMENT_DATA2;
+  columnsToDisplay2 = [];
+  expandedElement2;
+  columnName2 = [];
+  isMobile2 = false;
+  dataSource3 = ELEMENT_DATA3;
+  columnsToDisplay3 = [];
+  expandedElement3;
+  columnName3 = [];
+  isMobile3 = false;
   filterValue: string;
   constructor(private breakpointObserver: BreakpointObserver) {
     this.isHandset$.subscribe(res => {
@@ -50,6 +60,23 @@ export class BilllableBillingComponent implements OnInit {
         this.columnName1 = ["Item", "Procedure Code", "Modifier", "Units", "Charge", "Payment", "Balance", "Action"]
         this.columnsToDisplay1 = ['item', 'procedure_code', 'modifier', 'units', 'charge', 'payment', 'balance', 'action']
       }
+      this.isMobile2 = res;
+      if (res) {
+        this.columnName2 = ["", "File Name", "Action"]
+        this.columnsToDisplay2 = ['is_expand', 'file_name', "action"]
+      } else {
+        this.columnName2 = ["File Name", "Type", "Date", "Action"]
+        this.columnsToDisplay2 = ['file_name', 'type', 'date', "action"]
+      }
+
+      this.isMobile3 = res;
+      if (res) {
+        this.columnName3 = ["", "File Name", "Action"]
+        this.columnsToDisplay3 = ['is_expand', 'file_name', "action"]
+      } else {
+        this.columnName3 = ["File Name", "Action", "Date", "Recipients", "Download Sent Documents", "Download Proof of Service"]
+        this.columnsToDisplay3 = ['file_name', 'action', 'date', "recipients", "sent_document", "proof_of_service"]
+      }
     })
   }
 
@@ -57,12 +84,20 @@ export class BilllableBillingComponent implements OnInit {
   }
   expandId: any;
   expandId1: any;
+  expandId2: any;
+  expandId3: any;
   openElement(element) {
     if (this.isMobile) {
       this.expandId = element.id;
     }
     if (this.isMobile1) {
       this.expandId1 = element.id;
+    }
+    if (this.isMobile2) {
+      this.expandId2 = element.id;
+    }
+    if (this.isMobile3) {
+      this.expandId3 = element.id;
     }
   }
   applyFilter(filterValue: string) {
@@ -80,6 +115,23 @@ const ELEMENT_DATA = [
 
 ];
 const ELEMENT_DATA1 = [
-  { "id": 1384, "item": "UQME", "procedure_code": "ML 101", "modifier": "96", "units": "1", "charge": "2200.00", "payment": "0", "balance": "2200.00"},
+  { "id": 1384, "item": "UQME", "procedure_code": "ML 101", "modifier": "96", "units": "1", "charge": "2200.00", "payment": "0", "balance": "2200.00" },
+  { "id": 1384, "item": "UQME", "procedure_code": "ML 101", "modifier": "96", "units": "1", "charge": "2200.00", "payment": "0", "balance": "2200.00" },
+  { "id": 1384, "item": "UQME", "procedure_code": "ML 101", "modifier": "96", "units": "1", "charge": "2200.00", "payment": "0", "balance": "2200.00" },
+
+];
+const ELEMENT_DATA2 = [
+  { "id": 123, "file_name": "Finalized and Signed Report.pdf", "type": "Report", "date": "05-25-2019" },
+  { "id": 1, "file_name": "Submission Cover Letter", "type": "Attachment", "date": "05-25-2019" },
+  { "id": 2, "file_name": "Finalized and Signed Report.pdf", "type": "Report", "date": "05-25-2019" },
+  { "id": 3, "file_name": "Submission Cover Letter", "type": "Attachment", "date": "05-25-2019" },
+  { "id": 4, "file_name": "Finalized and Signed Report.pdf", "type": "Report", "date": "05-25-2019" },
+  { "id": 5, "file_name": "Submission Cover Letter", "type": "Attachment", "date": "05-25-2019" },
+
+];
+const ELEMENT_DATA3 = [
+  { "id": 6, "file_name": "Appointment Notification Letter", "action": "Mailed On Demand", "date": "05-25-2019", "recipients":"Claimant, Claims Adjuster, Applicant Attorney Defense Attorney, Employer, DEU Office", "sent_document":"Download","proof_of_service":"Download" },
+  { "id": 5, "file_name": "QME 110 - QME Appointment Notification Form", "action": "Downloaded", "date": "05-25-2019", "recipients":"", "sent_document":"Download","proof_of_service":"Download" },
+  { "id": 9, "file_name": "QME 122 - AME or QME Declaration of Service of…", "action": "Downloaded", "date": "05-25-2019", "recipients":"", "sent_document":"Download","proof_of_service":"Download" },
 
 ];
