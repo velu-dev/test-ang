1b19b91629f591b7933c5a7c6850fa98ec3934df
Sarath S <sarath.s@auriss.com>
2020-09-14 12:19:42 +0530

    Add hash in routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -43,7 +43,7 @@ const routes: Routes = [
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, { useHash: true ,scrollPositionRestoration: 'enabled' })],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
