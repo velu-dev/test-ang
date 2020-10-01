38c973656a8e595066c8e3724793280e77c6d0eb
Sarath S <sarath.s@auriss.com>
2020-10-01 18:44:35 +0530

    Number only add in billable item

--- src/app/subscriber/components/claims/new-billable-item/new-billable-item.component.ts
+++ src/app/subscriber/components/claims/new-billable-item/new-billable-item.component.ts
@@ -376,4 +376,13 @@ export class NewBillableItemComponent implements OnInit {
     this._location.back();
   }
 
+  numberOnly(event): boolean {
+    const charCode = (event.which) ? event.which : event.keyCode;
+    if (charCode > 31 && (charCode < 48 || charCode > 57)) {
+      return false;
+    }
+    return true;
+
+  }
+
 }
--- src/app/subscriber/examiner/appointment-details/appointment-details.component.ts
+++ src/app/subscriber/examiner/appointment-details/appointment-details.component.ts
@@ -797,6 +797,15 @@ columnName1 = [];
     }
 
   }
+
+  numberOnly(event): boolean {
+    const charCode = (event.which) ? event.which : event.keyCode;
+    if (charCode > 31 && (charCode < 48 || charCode > 57)) {
+      return false;
+    }
+    return true;
+
+  }
 }
 
 @Component({
