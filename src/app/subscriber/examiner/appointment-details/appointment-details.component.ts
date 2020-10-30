f8ff0e5723d79a9e52fca133c1a0a730bdd6c5c3
Sarath S <sarath.s@auriss.com>
2020-10-30 16:19:27 +0530

    Generate form hide changes

--- src/app/subscriber/examiner/appointment-details/appointment-details.component.ts
+++ src/app/subscriber/examiner/appointment-details/appointment-details.component.ts
@@ -151,7 +151,7 @@ export class AppointmentDetailsComponent implements OnInit {
     private intercom: IntercomService,
     private cookieService: CookieService
   ) {
-    this.userEmail = JSON.parse(this.cookieService.get('user')).sign_in_email_id.split('@').pop();
+    this.userEmail = JSON.parse(this.cookieService.get('user')).sign_in_email_id.toLowerCase();
     console.log(this.userEmail)
     this.intercom.setBillableItem("Billable Item");
     this.cookieService.set('billableItem', null)
