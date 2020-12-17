271fc563126fb4cee499f79dda94c59b46591725
Velusamy Venkatraman <velusamy.v@auriss.com>
2020-12-17 19:29:57 +0530

    Injury API changes completed

--- src/app/subscriber/components/claims/common/injury/injury.component.ts
+++ src/app/subscriber/components/claims/common/injury/injury.component.ts
@@ -411,18 +411,18 @@ export class InjuryPopup {
         };
         arrData.push(part)
       }
-      let count = 0;
-      arrData.map(row => {
-        // row.date_of_injury = moment(row.date_of_injury).format("MM-DD-YYYY")
-        this.claimService.updateInjury(row, this.claim_id).subscribe(res => {
-          this.dialogRef.close();
-          if (arrData.length == count)
-            this.alertService.openSnackBar(this.isEdit ? "Injury updated successfully" : "Claim Injury added successfully", 'success')
-        }, error => {
-          this.alertService.openSnackBar(error.error.message, "error")
-        })
-        count = count + 1;
+
+      // let count = 0;
+      // arrData.map(row => {
+      this.claimService.addInjury(arrData, this.claim_id).subscribe(res => {
+        // if (arrData.length == count)
+        this.dialogRef.close();
+        this.alertService.openSnackBar(this.isEdit ? "Injury updated successfully" : "Claim Injury added successfully", 'success')
+      }, error => {
+        this.alertService.openSnackBar(error.error.message, "error")
       })
+      //   count = count + 1;
+      // })
     }
   }
   changeEvent() {
