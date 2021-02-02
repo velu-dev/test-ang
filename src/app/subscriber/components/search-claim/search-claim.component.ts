b93f6bdeb128b118bba33227b7614e324b8925ff
Sarath S <sarath.s@auriss.com>
2021-02-02 11:06:17 +0530

    Search component create

---
+++ src/app/subscriber/components/search-claim/search-claim.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-search-claim',
+  templateUrl: './search-claim.component.html',
+  styleUrls: ['./search-claim.component.scss']
+})
+export class SearchClaimComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
