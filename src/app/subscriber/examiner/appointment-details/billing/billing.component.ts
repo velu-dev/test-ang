bea51f7291eb8507f603c65fb139686b9d2ae5d8
Sarath S <sarath.s@auriss.com>
2021-03-01 12:39:00 +0530

    Bill line item excess page changes

--- src/app/subscriber/examiner/appointment-details/billing/billing.component.ts
+++ src/app/subscriber/examiner/appointment-details/billing/billing.component.ts
@@ -784,6 +784,7 @@ export class BilllableBillingComponent implements OnInit {
 
   billing_line_items: any;
   newFeeScheduleStatus: boolean;
+  billingCodeDetails: any;
   getBillLineItem() {
     this.touchedRows = [];
     this.userTable = this.fb.group({
@@ -792,8 +793,9 @@ export class BilllableBillingComponent implements OnInit {
     this.billingService.getBillLineItem(this.paramsId.claim_id, this.paramsId.billId).subscribe(line => {
       if (line.data) {
         this.billing_line_items = line.data;
-        this.newFeeScheduleStatus = this.billing_line_items[0].is_new_fee_schedule;
         line.data.map((item, index) => {
+          this.newFeeScheduleStatus = this.billing_line_items[0].is_new_fee_schedule;
+          this.billingCodeDetails = this.billing_line_items[0].billing_code_details;
           let firstData = {};
           this.addRow(1);
           let modifier = item.modifier ? item.modifier.split('-') : [];
@@ -1032,7 +1034,7 @@ export class BilllableBillingComponent implements OnInit {
       group.get('unitTotal').patchValue(+(e * group.get('unit_price').value))
       group.get('charge').patchValue(+charge)
     } else if (group.value.is_excess_pages) {
-      let charge = +group.get('units').value * + this.billing_line_items[0].billing_code_details.rate_for_extra_units
+      let charge = +group.get('units').value * + this.billingCodeDetails.rate_for_extra_units
       group.get('charge').patchValue(+charge)
     }
   }
@@ -1042,11 +1044,10 @@ export class BilllableBillingComponent implements OnInit {
     if (val && val.trim() && this.newFeeScheduleStatus) {
       valReplace = val.replace(/[^a-zA-Z]/g, "");
       if (valReplace.toLowerCase() == 'mlprr') {
-        console.log(this.billing_line_items[0]);
         group.get('is_excess_pages').patchValue(true);
         group.get('modifierList').patchValue([]);
-        group.get('unitType').patchValue(this.billing_line_items[0].billing_code_details.extra_unit_type == 'page' ? 'Pages' : '');
-      }else{
+        group.get('unitType').patchValue(this.billingCodeDetails.extra_unit_type == 'page' ? 'Pages' : '');
+      } else {
         group.get('is_excess_pages').patchValue(false);
       }
     }
