af4b91fc9ff1a47b60573d89c97c66c64037e0cd
Venkatesan Mariyappan <venkatesan.m@auriss.com>
2021-04-08 10:31:28 +0530

    Changed header column name Due Date to Standing Due Date

--- src/app/subscriber/components/dashboard/dashboard.component.ts
+++ src/app/subscriber/components/dashboard/dashboard.component.ts
@@ -77,7 +77,7 @@ export class DashboardComponent implements OnInit {
         this.columnName = ["", "Claimant", "Critical"]
         this.columnsToDisplay = ['is_expand', 'claimant_first_name', "critical"]
       } else {
-        this.columnName = ["", "Claimant", "Date of Birth", "Examiner", "Exam Procedure Type", "Standing", "Date of Service /" + '\n' + "Date Item Received", "Due Date", "Critical"]
+        this.columnName = ["", "Claimant", "Date of Birth", "Examiner", "Exam Procedure Type", "Standing", "Date of Service /" + '\n' + "Date Item Received", "Standing Due Date", "Critical"]
         this.columnsToDisplay = ['is_expand', 'claimant_first_name', 'date_of_birth', 'examiner_first_name', "exam_procedure_name", "standing", 'appointment_scheduled_date_time', 'due_date', 'critical']
       }
     })
