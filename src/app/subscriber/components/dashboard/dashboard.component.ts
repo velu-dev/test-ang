2ca25104751f486fdfe04e42aef98451e9770abf
Velusamy Venkatraman <velusamy.v@auriss.com>
2021-05-21 14:51:22 +0530

    Dashboard expand collapse issue fixed in subscriber& Manager login

--- src/app/subscriber/components/dashboard/dashboard.component.ts
+++ src/app/subscriber/components/dashboard/dashboard.component.ts
@@ -26,9 +26,10 @@ export interface PeriodicElement {
   styleUrls: ['./dashboard.component.scss'],
   animations: [
     trigger('detailExpand', [
-      state('collapsed', style({ height: '0px', minHeight: '0' })),
+      state('collapsed, void', style({ height: '0px' })),
       state('expanded', style({ height: '*' })),
       transition('expanded <=> collapsed', animate('225ms cubic-bezier(0.4, 0.0, 0.2, 1)')),
+      transition('expanded <=> void', animate('225ms cubic-bezier(0.4, 0.0, 0.2, 1)'))
     ]),
   ],
 })
