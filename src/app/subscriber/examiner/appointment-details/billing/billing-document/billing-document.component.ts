354d5338dcfa76bb0ff372a0785386e7f20c6ed6
Venkatesan Mariyappan <venkatesan.m@auriss.com>
2021-06-10 17:04:35 +0530

    Billing documents table lable changes

--- src/app/subscriber/examiner/appointment-details/billing/billing-document/billing-document.component.ts
+++ src/app/subscriber/examiner/appointment-details/billing/billing-document/billing-document.component.ts
@@ -51,7 +51,7 @@ export class BillingDocumentComponent implements OnInit, OnDestroy {
         this.columnsNameDoc = ["", "File Name"]
         this.columnsToDisplayDoc = ['is_expand', 'file_name']
       } else {
-        this.columnsNameDoc = ["", "Ref #", "File Name", "Action", "Date", "Recipients", "Download" + '\n' + "Sent Documents", "Download On Demand" + '\n' + "Proof of Service", "Further Information"]
+        this.columnsNameDoc = ["", "Ref #", "File Name", "Type", "Date", "Recipients", "Download" + '\n' + "Sent Documents", "Download" + '\n' + "Proof of Service", "Further Information"]
         this.columnsToDisplayDoc = ['doc_image', 'request_reference_id', 'file_name', 'action', "date", "recipients", 'download', 'proof_of_service', 'payor_response_message']
       }
       this.isMobile = res;
