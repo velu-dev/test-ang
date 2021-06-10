bab1223710db784f11b8d9655c87f2467f7402f0
Velusamy Venkatraman <velusamy.v@auriss.com>
2021-06-10 13:40:34 +0530

    Drag and Drop file upload initiated

--- src/app/shared/components/file-upload/file-upload.component.ts
+++ src/app/shared/components/file-upload/file-upload.component.ts
@@ -9,12 +9,57 @@ import * as globals from '../../../globals';
 export class FileUploadComponent implements OnInit {
   info = globals.info
   alert = globals.alert
+
+  isDropZoneActive = false;
+  imageSource = "";
+  textVisible = true;
+  progressVisible = false;
+  progressValue = 0;
+
+
   constructor(
     public dialog: MatDialog,
     public dialogRef: MatDialogRef<FileUploadComponent>,
     @Inject(MAT_DIALOG_DATA) public data: any) {
     dialogRef.disableClose = true;
+    this.onDropZoneEnter = this.onDropZoneEnter.bind(this);
+    this.onDropZoneLeave = this.onDropZoneLeave.bind(this);
+    this.onUploaded = this.onUploaded.bind(this);
+    this.onProgress = this.onProgress.bind(this);
+    this.onUploadStarted = this.onUploadStarted.bind(this);
   }
+  onDropZoneEnter(e) {
+    if (e.dropZoneElement.id === "dropzone-external")
+      this.isDropZoneActive = true;
+  }
+
+  onDropZoneLeave(e) {
+    if (e.dropZoneElement.id === "dropzone-external")
+      this.isDropZoneActive = false;
+  }
+
+  onUploaded(e) {
+    const file = e.file;
+    const fileReader = new FileReader();
+    fileReader.onload = () => {
+      this.isDropZoneActive = false;
+      this.imageSource = fileReader.result as string;
+    }
+    fileReader.readAsDataURL(file);
+    this.textVisible = false;
+    this.progressVisible = false;
+    this.progressValue = 0;
+  }
+
+  onProgress(e) {
+    this.progressValue = e.bytesLoaded / e.bytesTotal * 100;
+  }
+
+  onUploadStarted() {
+    this.imageSource = "";
+    this.progressVisible = true;
+  }
+
   ngOnInit() {
 
   }
