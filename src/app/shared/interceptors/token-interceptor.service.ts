32413d987ec1b037b6ea3472f41db0cc10f53950
Sarath S <sarath.s@auriss.com>
2021-07-05 15:14:18 +0530

    User role mismatch changes

--- src/app/shared/interceptors/token-interceptor.service.ts
+++ src/app/shared/interceptors/token-interceptor.service.ts
@@ -8,6 +8,8 @@ import { catchError } from "rxjs/operators";
 import { CookieService } from '../services/cookie.service';
 import { Router } from '@angular/router';
 import * as moment from 'moment';
+import { CognitoService } from '../services/cognito.service';
+import { AlertService } from '../services/alert.service';
 export const InterceptorSkipHeader = "X-Skip-Interceptor";
 
 @Injectable({
@@ -15,7 +17,8 @@ export const InterceptorSkipHeader = "X-Skip-Interceptor";
 })
 export class TokenInterceptorService implements HttpInterceptor {
   roleId: any;
-  constructor(private cookieService: CookieService, private router: Router) {
+  constructor(private cookieService: CookieService, private router: Router,
+    private cognitoService: CognitoService, private alertService: AlertService) {
 
   }
   intercept(
@@ -39,7 +42,7 @@ export class TokenInterceptorService implements HttpInterceptor {
           const headers: { [name: string]: string | string[] } = {};
           headers["Authorization"] = "Bearer " + token['idToken'].jwtToken;
           headers["role"] = this.roleId;
-          headers["x-simplexam-timezone"] =  moment && moment.tz && moment.tz.guess() ? moment.tz.guess() : '';
+          headers["x-simplexam-timezone"] = moment && moment.tz && moment.tz.guess() ? moment.tz.guess() : '';
           const newHeader = new HttpHeaders(headers);
           const reqClone = req.clone({
             headers: newHeader
@@ -51,17 +54,31 @@ export class TokenInterceptorService implements HttpInterceptor {
       .pipe(
         catchError((error: HttpErrorResponse) => {
           let err: any = error;
-
+          console.log(err)
           if (err && err == 'No current user') {
             let url = location.href.split('/').pop();
             if (url != 'verification') {
               this.router.navigate(['/login']);
             }
+          }
 
+          if (err && err.status && err.status == 401 && err.error && err.error.is_logout) {
+            this.alertService.openSnackBar("Logged in user role mismatch!", "error")
+            this.logout();
           }
 
           return throwError(error);
         })
       );
   }
+
+  logout() {
+    this.cognitoService.logOut().subscribe(response => {
+      this.cookieService.deleteAll();
+      localStorage.clear();
+      this.router.navigate(['/'])
+    }, error => {
+    })
+  }
 }
+
