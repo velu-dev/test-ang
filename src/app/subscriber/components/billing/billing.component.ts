9e9d7310be9bd47f2c1684bd97f9dc9e2dd43db1
Sarath S <sarath.s@auriss.com>
2021-08-26 17:34:17 +0530

    navigation by submission type from billing

--- src/app/subscriber/components/billing/billing.component.ts
+++ src/app/subscriber/components/billing/billing.component.ts
@@ -122,7 +122,7 @@ export class BillingComponent implements OnInit {
     this.cookieService.set('billNo', e.bill_no)
 
     //this.router.navigateByUrl(this.router.url + '/' + e.claim_id + '/' + e.billable_item_id + '/' + 'edit-billing' + '/' + e.bill_id)
-    this.router.navigateByUrl(this.router.url + '/claimant/' + e.claimant_id + '/claim/' + e.claim_id + '/billable-item/' + e.billable_item_id + '/billing/' + e.bill_id)
+    this.router.navigateByUrl(this.router.url + '/claimant/' + e.claimant_id + '/claim/' + e.claim_id + '/billable-item/' + e.billable_item_id + '/billing/' + e.bill_id + '/' + e.bill_submission_type.toLowerCase())
   }
 
 
